4928d8ffba66481c1353a149c80614084a3845a9
bp-statar001 <statarinov@productengine.com>
2023-08-10 17:46:26 +0300

    First commit

---
+++ src/main/java/lesson41/Test.java
@@ -0,0 +1,22 @@
+package lesson41;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        /* (3)
+        Cat cat = new Cat();
+        Dog dog = new Dog();
+        dog.makeSound();
+        cat.makeSound();
+        dog.eat();
+        cat.eat();
+        */
+
+        // Animal animal = new Animal(); (1)
+
+
+
+    }
+
+}
---
+++ src/main/java/lesson45/ReadObject.java
@@ -0,0 +1,31 @@
+package lesson45;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+public class ReadObject {
+
+    public static void main(String[] args) {
+
+        try {
+            FileInputStream fis = new FileInputStream("people.bin");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+
+            Person person1 = (Person) ois.readObject();
+            Person person2 = (Person) ois.readObject();
+
+            System.out.println(person1);
+            System.out.println(person2);
+
+            ois.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
